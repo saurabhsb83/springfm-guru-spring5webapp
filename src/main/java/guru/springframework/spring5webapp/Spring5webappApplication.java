b17e845288ca2bfe2d46aece02d786e739a84f91
Saurabh <saurabh.sb83@gmail.com>
2022-08-21 11:19:16 +0530

    added the domain classes and the repository interfaces

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,12 +1,39 @@
 package guru.springframework.spring5webapp;
 
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Spring5webappApplication {
 
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Autowired
+	private PublisherRepository publisherRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
+		System.out.println("Hello World !");
 	}
+
+
 }
+
+
+
+
